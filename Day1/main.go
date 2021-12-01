be422a3a07e0019dfdafa63dfdf24dfa67365095
Tim <timatlee@gmail.com>
2021-11-30 22:21:07 -0700

    Update main.go

--- Day1/main.go
+++ Day1/main.go
@@ -5,34 +5,33 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
-
-	fmt.Println("go" + "lang")
-
-	fmt.Println("1+1 =", 1+1)
-	fmt.Println("7.0/3.0 =", 7.0/3.0)
-
-	fmt.Println(true && false)
-	fmt.Println(true || false)
-	fmt.Println(!true)
+	numbers := readfile()
+	fmt.Println(numbers)
 }
 
-func readfile() {
+func readfile() []int {
 	file, err := os.Open("day1/testinput.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
+	var numbers []int
+
 	scanner := bufio.NewScanner(file)
 	// optionally, resize scanner's capacity for lines over 64K, see next example
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		intVar, _ := strconv.Atoi(scanner.Text())
+		numbers = append(numbers, intVar)
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+
+	return numbers
 }
