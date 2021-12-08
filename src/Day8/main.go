0cc3ee8a567856f879f615ab487215ab9cf50f21
Tim AtLee <timatlee@gmail.com>
2021-12-08 08:09:45 -0700

    Day 8 WIP.

---
+++ src/Day8/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/montanaflynn/stats"
+)
+
+func main() {
+	//filecontent := readfile("testinput.txt")
+	filecontent := readfile("day8input.txt")
+	inputStrings := strings.Split(filecontent[0], ",")
+
+}
+
+
+func readfile(filename string) []string {
+	dir, _ := os.Getwd()
+
+	file, err := os.Open(dir + "/" + filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var commands []string
+
+	scanner := bufio.NewScanner(file)
+	// optionally, resize scanner's capacity for lines over 64K, see next example
+	for scanner.Scan() {
+		commands = append(commands, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return commands
+}
