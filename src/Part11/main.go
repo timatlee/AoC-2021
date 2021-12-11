d75cc2a3dd621387d5144eb40713b4b5ab92b464
Tim AtLee <timatlee@gmail.com>
2021-12-11 21:03:39 +0000

    Day 11 Part 1 done.

--- src/Part11/main.go
+++ src/Part11/main.go
@@ -14,19 +14,79 @@ type Octopus struct {
 }
 
 func main() {
-	filecontent := readfile("test1input.txt")
-	//filecontent := readfile("day10input.txt")
+	//filecontent := readfile("test1input.txt")
+	//filecontent := readfile("testinput.txt")
+	filecontent := readfile("day11input.txt")
 
 	octopus := parseInput(filecontent)
+	// Inital state
 	PrintOctopus(octopus)
-	increaseEnergy(octopus)
+	var totalFlashes int
 
+	for i := 0; i < 100; i++ {
+		totalFlashes += stepOne(octopus)
+		PrintOctopus(octopus)
+	}
+	println(totalFlashes)
+
+}
+
+func stepOne(o [][]Octopus) int {
+	// FIrst phase: Increase the energy level on all O's.
+	increaseEnergy(o)
+
+	newFlashes := 1
+	totalFlashes := 0
+	for newFlashes > 0 {
+		newFlashes = 0
+
+		for y := 0; y < len(o); y++ {
+			for x := 0; x < len(o[y]); x++ {
+				if o[y][x].Energy > 9 && !o[y][x].HasFlashed {
+					newFlashes += 1
+					o[y][x].HasFlashed = true
+					increaseAdjacents(o, x, y)
+				}
+			}
+		}
+		totalFlashes += newFlashes
+	}
+	// Cleanup.
+	for y := 0; y < len(o); y++ {
+		for x := 0; x < len(o[y]); x++ {
+			if o[y][x].Energy > 9 {
+				o[y][x].Energy = 0
+				o[y][x].HasFlashed = false
+			}
+		}
+	}
+	return totalFlashes
+}
+
+func increaseAdjacents(o [][]Octopus, x int, y int) int {
+	var newFlashes int = 0
+	for a := -1; a <= 1; a++ { // Y offset
+		for b := -1; b <= 1; b++ { // X offset
+			if !(a == 0 && b == 0) { // Check that we're not in the middle.
+				// Check that we're within Y bounds.
+				if (y+a >= 0) && (y+a < len(o)) {
+					// Check that we're within the X bounds
+					if (x+b >= 0) && (x+b < len(o[y])) {
+						// Increase this offset'd point. If it's > 9, indicate a flash.
+						o[y+a][x+b].Energy++
+					}
+				}
+			}
+		}
+	}
+	return newFlashes
 }
 
 func PrintOctopus(o [][]Octopus) {
 	for y := 0; y < len(o); y++ {
 		fmt.Println(o[y])
 	}
+	fmt.Println()
 
 }
 
@@ -34,7 +94,6 @@ func increaseEnergy(o [][]Octopus) {
 	for y := 0; y < len(o); y++ {
 		for x := 0; x < len(o[y]); x++ {
 			o[y][x].Energy += 1
-			o[y][x].HasFlashed = false
 		}
 	}
 }
@@ -47,8 +106,8 @@ func parseInput(f []string) [][]Octopus {
 	for y := 0; y < len(f); y++ {
 		for x := 0; x < len(f[y]); x++ {
 			digit, _ := strconv.Atoi(string(f[y][x]))
-			o[x][y].Energy = digit
-			o[x][y].HasFlashed = false
+			o[y][x].Energy = digit
+			o[y][x].HasFlashed = false
 		}
 	}
 
